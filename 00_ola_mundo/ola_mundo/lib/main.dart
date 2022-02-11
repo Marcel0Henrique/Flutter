651e2f027022f0aefee3fb2fe5440bc953b4df0d
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-02-11 16:21:33 -0300

    create flutter project ola_mundo

---
+++ 00_ola_mundo/ola_mundo/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(
+        color: Colors.black,
+        alignment: Alignment.center,
+        child: const Text('Olá Mundo!'),
+      ),
+    );
+  }
+}
