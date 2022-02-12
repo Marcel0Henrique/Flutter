bd37c413ab45c09f80e637c751ba44df7c234013
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-02-12 00:21:22 -0300

    update

--- 01_contador_de_pessoas/contador/lib/main.dart
+++ 01_contador_de_pessoas/contador/lib/main.dart
@@ -4,17 +4,24 @@ void main() {
   runApp(const MyApp());
 }
 
+//* Criando o widget MyAPP
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Container(
-        color: Colors.black,
-        alignment: Alignment.center,
-        child: const Text("Ola Mundo"),
-      ),
+    return const MaterialApp(home: HomePage());
+  }
+}
+
+//* Criando o Widge HomePage que é a pagina principal
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
     );
   }
 }
