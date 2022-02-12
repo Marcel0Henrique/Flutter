5bbfe4532775cab0b439979a8cb1ca81b67796bb
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-02-12 17:53:38 -0300

    update

--- 02_login_system/login_system/lib/main.dart
+++ 02_login_system/login_system/lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(const MyApp());
 }
 
+//*Criando Widget MyApp
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -16,13 +17,24 @@ class MyApp extends StatelessWidget {
   }
 }
 
+//* Criando a tela de login
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //? Cor de fundo
       backgroundColor: Colors.blue[600],
+
+      //? criando coluna para colocar os widgets
+      body: Column(
+        //? ALinhando os widgets de forma centralizada
+        mainAxisAlignment: MainAxisAlignment.center,
+
+        //*Adicionando widgets
+        children: [Text('Sign In')],
+      ),
     );
   }
 }
