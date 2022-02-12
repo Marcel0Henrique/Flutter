6edb2291ef94dba5b27951ce4ea29be2814167e1
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-02-12 12:39:15 -0300

    update

--- 01_contador_de_pessoas/contador/lib/main.dart
+++ 01_contador_de_pessoas/contador/lib/main.dart
@@ -34,101 +34,112 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 99, 3, 167),
+      backgroundColor: const Color.fromARGB(255, 99, 3, 167),
 
       //? Criando o APPBar
       appBar: AppBar(
         title: const Text("Contador",
             style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold)),
-        backgroundColor: Color.fromARGB(255, 119, 12, 4),
+        backgroundColor: const Color.fromARGB(255, 119, 12, 4),
       ),
 
       //? Criando o corpo do app como uma coluna
-      body: Column(
-        //* Centralizando
-        mainAxisAlignment: MainAxisAlignment.center,
-
-        //? definindo os itens que vão ter dentro do corpo
-        children: [
-          const Text(
-            "Liberado",
-            style: TextStyle(
-              color: Color.fromARGB(255, 19, 124, 23),
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2,
-            ),
+      //? teve que colocar dentro de um Container para poder adicionar a imagem de fundo
+      body: Container(
+        //* Adicionando imagem de fundo
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/image.jpg'),
+            //? Manipulando imagem para preencher a tela
+            fit: BoxFit.cover,
           ),
+        ),
 
-          //* Criando espaçamento usando o Padding
-          const Padding(
-            padding: EdgeInsets.all(50),
-            child: Text(
-              "0",
+        child: Column(
+          //* Centralizando
+          mainAxisAlignment: MainAxisAlignment.center,
+
+          //? definindo os itens que vão ter dentro do corpo
+          children: [
+            const Text(
+              "Entrada Liberada",
               style: TextStyle(
-                fontSize: 80,
+                color: Color.fromARGB(255, 0, 175, 6),
+                fontSize: 30,
                 fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 0, 0, 0),
               ),
             ),
-          ),
 
-          //*Criando Row e os botoes
-          Row(
-            //* Centralizando
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  fixedSize: const Size(90, 90),
-                  //? Muda a cor quando pressiona o botão
-                  primary: Colors.black,
-                  //? Arredondando a borda
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16),
-                  ),
-                ),
-                onPressed: decrement,
-                child: const Text(
-                  "Saiu",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
+            //* Criando espaçamento usando o Padding
+            const Padding(
+              padding: EdgeInsets.all(50),
+              child: Text(
+                "0",
+                style: TextStyle(
+                  fontSize: 80,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 0, 0, 0),
                 ),
               ),
+            ),
 
-              //? Foi usado o SizedBox para dar o espaçamento entre os botoes
-              const SizedBox(
-                width: 35,
-              ),
-
-              TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  fixedSize: const Size(90, 90),
-                  //? Muda a cor quando pressiona o botão
-                  primary: Colors.black,
-                  //? Arredondando a borda
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16),
+            //*Criando Row e os botoes
+            Row(
+              //* Centralizando
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    fixedSize: const Size(90, 90),
+                    //? Muda a cor quando pressiona o botão
+                    primary: Colors.black,
+                    //? Arredondando a borda
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                  ),
+                  onPressed: decrement,
+                  child: const Text(
+                    "Saiu",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-                onPressed: increment,
-                child: const Text(
-                  "Entrou",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
+
+                //? Foi usado o SizedBox para dar o espaçamento entre os botoes
+                const SizedBox(
+                  width: 35,
+                ),
+
+                TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    fixedSize: const Size(90, 90),
+                    //? Muda a cor quando pressiona o botão
+                    primary: Colors.black,
+                    //? Arredondando a borda
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                  ),
+                  onPressed: increment,
+                  child: const Text(
+                    "Entrou",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
