fa31a48d419e5e92dcce6b30a5a4cea199b9ca77
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-02-12 21:31:27 -0300

    update

--- 02_tic_tac_toe/tic_tac_toe/lib/main.dart
+++ 02_tic_tac_toe/tic_tac_toe/lib/main.dart
@@ -1,1 +1,36 @@
-void main() {}
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: true,
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [],
+      ),
+    );
+  }
+}
