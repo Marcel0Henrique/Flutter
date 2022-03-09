2c35a306ad92a10fc87c26d941800317d355db9d
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-03-09 15:26:41 -0300

    update

--- lista_de_tarefas/lista_de_tarefas/lib/views/home.dart
+++ lista_de_tarefas/lista_de_tarefas/lib/views/home.dart
@@ -5,8 +5,31 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.amber[900],
+    return const Scaffold(
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.only(
+            left: 20,
+            right: 20,
+          ),
+          child: TextField(
+            decoration: InputDecoration(
+              labelText: 'Email',
+              hintText: 'exemplo@email.com',
+              border: OutlineInputBorder(),
+              errorText: null,
+              prefixIcon: Icon(Icons.email_outlined),
+            ),
+            //? obscuretext oculta os caracteres que foram digitados
+            //*obscureText: true,
+            //? Mostra um teclado voltado para digitar email
+            keyboardType: TextInputType.emailAddress,
+            style: TextStyle(
+              fontSize: 17,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
