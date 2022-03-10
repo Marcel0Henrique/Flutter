1b3d95cbd8befddda75ccae64579c5e754b8bfe2
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-03-10 01:28:18 -0300

    update

--- lista_de_tarefas/lista_de_tarefas/lib/views/home.dart
+++ lista_de_tarefas/lista_de_tarefas/lib/views/home.dart
@@ -1,35 +1,55 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  HomePage({Key? key}) : super(key: key);
+
+  //* Controlador para poder pegar as informações do campo
+  final TextEditingController emailController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
         child: Padding(
-          padding: EdgeInsets.only(
+          padding: const EdgeInsets.only(
             left: 20,
             right: 20,
+            top: 25,
           ),
-          child: TextField(
-            decoration: InputDecoration(
-              labelText: 'Email',
-              hintText: 'exemplo@email.com',
-              border: OutlineInputBorder(),
-              errorText: null,
-              prefixIcon: Icon(Icons.email_outlined),
-            ),
-            //? obscuretext oculta os caracteres que foram digitados
-            //*obscureText: true,
-            //? Mostra um teclado voltado para digitar email
-            keyboardType: TextInputType.emailAddress,
-            style: TextStyle(
-              fontSize: 17,
-            ),
+          child: Column(
+            //? faz a coluna ocupar o espaço minimo
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                //? Adicionando o controlador ao widget
+                controller: emailController,
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                  hintText: 'exemplo@email.com',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.email_outlined),
+                ),
+                //? obscuretext oculta os caracteres que foram digitados
+                //*obscureText: true,
+                //? Mostra um teclado voltado para digitar email
+                keyboardType: TextInputType.emailAddress,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: ElevatedButton(
+                  onPressed: login,
+                  child: const Text('Entrar'),
+                ),
+              ),
+            ],
           ),
         ),
       ),
     );
+  }
+
+  void login() {
+    print(emailController.text);
+    emailController.clear();
   }
 }
