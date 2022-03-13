ee857367b85f4081112a0bc86998735c838cdff8
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-03-12 23:50:25 -0300

    update

--- lista_de_tarefas/lista_de_tarefas/lib/views/home.dart
+++ lista_de_tarefas/lista_de_tarefas/lib/views/home.dart
@@ -10,44 +10,65 @@ class HomePage extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.symmetric(
             //? Adiciona um espaçamento de forma horizontal, na esqueda e na direita
-            horizontal: 16,
+            horizontal: 10,
           ),
-          child: Row(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
-              //* o Expanded tenta ocupar o maior espaço disponivel, sem ele acontece um erro por causa do Row
-              const Expanded(
-                child: TextField(
-                  decoration: InputDecoration(
-                    labelText: "Adicione uma tarefa",
-                    hintText: "Ex: Ler um livro",
-                    border: OutlineInputBorder(),
+              Row(
+                children: [
+                  //* o Expanded tenta ocupar o maior espaço disponivel, sem ele acontece um erro por causa do Row
+                  const Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: "Adicione uma tarefa",
+                        hintText: "Ex: Ler um livro",
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
                   ),
-                ),
-              ),
 
-              //? Cria um espaço em branco entre o TextFiel e o ElevatedButton
-              const SizedBox(
-                width: 8,
+                  //? Cria um espaço em branco entre o TextFiel e o ElevatedButton
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: const Color.fromARGB(255, 0, 197, 232),
+                      padding: const EdgeInsets.all(20),
+                    ),
+                    child: const Icon(
+                      Icons.add,
+                      size: 20,
+                    ),
+                  )
+                ],
               ),
-              ElevatedButton(
-                onPressed: teste,
-                style: ElevatedButton.styleFrom(
-                  primary: const Color.fromARGB(255, 0, 197, 232),
-                  padding: const EdgeInsets.all(20),
-                ),
-                child: const Icon(
-                  Icons.add,
-                  size: 20,
-                ),
+              const SizedBox(
+                height: 16,
+              ),
+              Row(
+                children: [
+                  const Expanded(
+                    child: Text("Você possui 0 tarefas pendentes"),
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Limpar Tudo'),
+                    style: ElevatedButton.styleFrom(
+                      primary: const Color.fromARGB(255, 0, 197, 232),
+                    ),
+                  )
+                ],
               )
             ],
           ),
         ),
       ),
     );
-  }
-
-  void teste() {
-    print('Teste');
   }
 }
