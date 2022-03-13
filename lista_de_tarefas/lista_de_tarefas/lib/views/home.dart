3a652d8533cc3fdc756b8ece971f9938736ff3f9
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-03-12 23:05:02 -0300

    update

--- lista_de_tarefas/lista_de_tarefas/lib/views/home.dart
+++ lista_de_tarefas/lista_de_tarefas/lib/views/home.dart
@@ -8,7 +8,10 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
+          padding: const EdgeInsets.symmetric(
+            //? Adiciona um espaçamento de forma horizontal, na esqueda e na direita
+            horizontal: 16,
+          ),
           child: Row(
             children: [
               //* o Expanded tenta ocupar o maior espaço disponivel, sem ele acontece um erro por causa do Row
@@ -28,7 +31,14 @@ class HomePage extends StatelessWidget {
               ),
               ElevatedButton(
                 onPressed: teste,
-                child: const Text('+'),
+                style: ElevatedButton.styleFrom(
+                  primary: const Color.fromARGB(255, 0, 197, 232),
+                  padding: const EdgeInsets.all(20),
+                ),
+                child: const Icon(
+                  Icons.add,
+                  size: 20,
+                ),
               )
             ],
           ),
